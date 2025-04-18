ea7595ec302fbfc0b96d381d69c81f6ddcd44538
Shaked Cohen <shakedbaws@yahoo.com>
2025-04-18 20:55:46 +0300

    admin hebrew ready

--- frontend/admin.py
+++ frontend/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from .models import Battle
 
 class BattleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'date', 'country')  # Fields to display in the admin list view
-    search_fields = ('title', 'country','date')  # Fields to search in the admin interface
-    
+    list_display = ('title', 'hebrew_country', 'year', 'month')  # Display in the list view
+    search_fields = ('title', 'hebrew_country', 'country', 'year', 'month')
+    list_filter = ('year', 'month', 'country')
+    exclude = ('hebrew_country',)  # Hide from the admin form
 
-# Register your models here.
-admin.site.register(Battle, BattleAdmin) #it tells django that this model should be added to the administration site.
-# This allows you to manage the Battle model through the Django admin interface.
+admin.site.register(Battle, BattleAdmin)
--- frontend/models.py
+++ frontend/models.py
@@ -1,6 +1,20 @@
+import json
 from django.db import models
+from pathlib import Path
 
-# Create your models here.
+# Load country data from countries.geojson
+geojson_path = Path(__file__).resolve().parent / 'static' / 'frontend' / 'data' / 'countries.geojson'
+with open(geojson_path, encoding='utf-8') as f:
+    geojson_data = json.load(f)
+    COUNTRY_CHOICES = sorted(
+        [(feature['properties']['name'], feature['properties']['name']) for feature in geojson_data['features']],
+        key=lambda x: x[0]
+    )
+
+# Load Hebrew country data from countriesHE.geojson
+hebrew_geojson_path = Path(__file__).resolve().parent / 'static' / 'frontend' / 'data' / 'countriesHE.geojson'
+with open(hebrew_geojson_path, encoding='utf-8') as f:
+    hebrew_geojson_data = json.load(f)
 
 class Battle(models.Model):
     MONTH_CHOICES = [
@@ -16,15 +30,37 @@ class Battle(models.Model):
     description = models.TextField()
     month = models.IntegerField(choices=MONTH_CHOICES)
     year = models.IntegerField(choices=YEAR_CHOICES)
-    country = models.CharField(max_length=100)
-    hebrew_country = models.CharField(max_length=100)
-
-
+    country = models.CharField(max_length=100, choices=COUNTRY_CHOICES)  # Dropdown for countries
+    hebrew_country = models.CharField(max_length=100, blank=True)  # Auto-filled field
 
     def __str__(self):
         return self.title
 
-    @property
-    def date(self):
-        # Dynamically construct the date from month and year
-        return f"{self.get_month_display()} {self.year}"
+    def save(self, *args, **kwargs):
+        print(f"Selected country: {self.country}")
+
+        # Find the unique identifiers (ISO3166-1-Alpha-3 and ISO3166-1-Alpha-2) of the selected country in countries.geojson
+        selected_country_id_alpha_3 = None
+        selected_country_id_alpha_2 = None
+        for feature in geojson_data['features']:
+            if feature['properties']['name'] == self.country:
+                selected_country_id_alpha_3 = feature['properties'].get('ISO3166-1-Alpha-3')  # ISO Alpha-3 code
+                selected_country_id_alpha_2 = feature['properties'].get('ISO3166-1-Alpha-2')  # ISO Alpha-2 code
+                break
+        print(f"Selected country ISO3166-1-Alpha-3: {selected_country_id_alpha_3}")
+        print(f"Selected country ISO3166-1-Alpha-2: {selected_country_id_alpha_2}")
+
+        # Match the identifiers with the corresponding entry in countriesHE.geojson
+        if selected_country_id_alpha_3 and selected_country_id_alpha_2:
+            for feature in hebrew_geojson_data['features']:
+                if (
+                    feature['properties'].get('ISO3166-1-Alpha-3') == selected_country_id_alpha_3 and
+                    feature['properties'].get('ISO3166-1-Alpha-2') == selected_country_id_alpha_2
+                ):
+                    self.hebrew_country = feature['properties'].get('name', '')  # Use 'name' for Hebrew name
+                    break
+
+        print(f"Auto-filled hebrew_country: {self.hebrew_country}")
+
+        super().save(*args, **kwargs)
+
