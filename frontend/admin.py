89bd1b52552e3aa80607fd1e2e0a4b431e03454d
Shaked Cohen <shakedbaws@yahoo.com>
2025-05-29 15:59:07 +0300

    pictures in the admin display

--- frontend/admin.py
+++ frontend/admin.py
@@ -8,25 +8,31 @@ class BattleAdmin(admin.ModelAdmin):
     list_filter = ('year', 'month', 'end_year', 'end_month', 'country')
     exclude = ('hebrew_country',)  # Hide from the admin form
     actions = ['duplicate_battle']
+
 #id,hebrew_name,קישור לתמונה,פרטים אישיים,קורות חיים,english_name,personal details,Biography
 @admin.register(Soldier)
 class SoldierAdmin(admin.ModelAdmin):
     list_display = (
         'id',
-        'hebrew_name','hebrew_personal_details', 'hebrew_biography'
-        ,'image_link'
-        ,'english_name','personal_details','Biography'
+        'hebrew_name', 'hebrew_personal_details', 'hebrew_biography',
+        'image_with_link',
+        'english_name', 'personal_details', 'Biography'
     )
-    search_fields = ('id','hebrew_name' ,'english_name')
-    list_filter = ('hebrew_name','english_name')
-    readonly_fields = ('image_link',)
+    search_fields = ('id', 'hebrew_name', 'english_name')
+    list_filter = ('hebrew_name', 'english_name')
+    readonly_fields = ('image_with_link',)
 
-
-    def image_preview(self, obj):
-        if obj.image_url:
-            return format_html('<img src="{}" style="max-height: 100px;"/>', obj.image_url)
+    def image_with_link(self, obj):
+        if obj.image_link:
+            return format_html(
+                '<div style="display: flex; align-items: center;">'
+                '<img src="{url}" style="max-height: 60px;"/>'
+                '<a href="{url}" target="_blank" style="margin-right: 10px;">{url}</a>'
+                '</div>',
+                url=obj.image_link
+            )
         return "-"
-    image_preview.short_description = 'תצוגת תמונה'
+    image_with_link.short_description = 'תמונה + קישור'
 
     def highlight_battle(self, obj):
         # Highlight if the battle has an end date
